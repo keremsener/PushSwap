fb5a1b5e6b1381505c626854388302991b9aedc3
ksener <blogkeremsener@gmail.com>
2026-03-04 12:27:27 +0300

    Fixed: Norm in main.c

--- main.c
+++ main.c
@@ -6,70 +6,77 @@
 /*   By: ksener <ksener@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 11:47:20 by ksener            #+#    #+#             */
-/*   Updated: 2026/03/04 11:43:08 by ksener           ###   ########.fr       */
+/*   Updated: 2026/03/04 12:24:54 by ksener           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdlib.h>
 
-void    test_print(t_list *a_head, t_list *b_head)
+void	test_print(t_list *a_head, t_list *b_head)
 {
-    int wlcount = 8;
+	int	wlcount;
 
-    printf("\n\na b\n___\n");
-    while (wlcount--)
-    {
-        if (a_head != NULL)
-        {
-            printf("%d", *(int *)a_head->content);
-            a_head = a_head->next;
-        }
-        else
-            printf(" ");
-        if (b_head != NULL)
-        {
-            printf(" %d", *(int *)b_head->content);
-            b_head = b_head->next;
-        }
-        else
-            printf(" ");
-        printf("\n");
-    }
+	wlcount = 8;
+	printf("\n\na b\n___\n");
+	while (wlcount--)
+	{
+		if (a_head != NULL)
+		{
+			printf("%d", *(int *)a_head->content);
+			a_head = a_head->next;
+		}
+		else
+			printf(" ");
+		if (b_head != NULL)
+		{
+			printf(" %d", *(int *)b_head->content);
+			b_head = b_head->next;
+		}
+		else
+			printf(" ");
+		printf("\n");
+	}
 }
 
-int main(int argc, char *argv[])
+void	split_args(int argc, char *argv[], t_list **a_head)
 {
-    t_list  *a_head;
-    t_list  *b_head;
-    int     i;
-    int     j;
-    char    **split_args;
-    int     *val;
+	int		i;
+	int		j;
+	int		*val;
+	char	**split_args;
 
-    if (argc < 2)
-        return (0);
-    i = 1;
-    a_head = NULL;
-    b_head = NULL;
-    while (i < argc)
-    {
-        split_args = ft_split(argv[i], ' ');
-        j = 0;
-        while (split_args[j])
-        {
-            val = (int *)malloc(sizeof(int));
-            if (!val)
-                return (1);
-            *val = ft_atoi(split_args[j]);
-            ft_lstadd_back(&a_head, ft_lstnew(val));
-            free(split_args[j]);
-            j++;
-        }
-        free(split_args);
-        i++;
-    }
-    insertion_sort(&a_head, &b_head);
-    test_print(a_head, b_head);
-    return (0);
+	i = 1;
+	while (i < argc)
+	{
+		split_args = ft_split(argv[i], ' ');
+		j = 0;
+		while (split_args[j])
+		{
+			val = (int *)malloc(sizeof(int));
+			if (!val)
+				return ;
+			*val = ft_atoi(split_args[j]);
+			ft_lstadd_back(&(*a_head), ft_lstnew(val));
+			free(split_args[j]);
+			j++;
+		}
+		free(split_args);
+		i++;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	t_list	*a_head;
+	t_list	*b_head;
+
+	if (argc < 2)
+		return (0);
+	a_head = NULL;
+	b_head = NULL;
+	split_args(argc, argv, &a_head);
+	insertion_sort(&a_head, &b_head);
+	test_print(a_head, b_head);
+	return (0);
 }
