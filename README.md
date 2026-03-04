e5a85a6e3336bec8cd3c4109152de6c019f1db78
ksener <blogkeremsener@gmail.com>
2026-03-04 11:52:53 +0300

    Edited: main.c and README.md | Added: ft_split in main.c
    Devoloped and modified parse system. Stronged main.c with split func

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 https://www.geeksforgeeks.org/dsa/insertion-sort-algorithm/
 https://www.geeksforgeeks.org/dsa/selection-sort-algorithm-2/
 https://www.geeksforgeeks.org/dsa/bubble-sort-algorithm/
+https://stackoverflow.com/questions/9642732/parsing-command-line-arguments-in-c
--- main.c
+++ main.c
@@ -3,84 +3,73 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adede <adede@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
+/*   By: ksener <ksener@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 11:47:20 by ksener            #+#    #+#             */
-/*   Updated: 2026/03/03 17:22:19 by adede            ###   ########.fr       */
+/*   Updated: 2026/03/04 11:43:08 by ksener           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdlib.h>
 
-void	test_print(t_list *a_head, t_list *b_head)
+void    test_print(t_list *a_head, t_list *b_head)
 {
-	int wlcount = 8;
+    int wlcount = 8;
 
-	printf("\n\na b\n___\n");
-	while (wlcount--)
-	{
-
-		if (a_head != NULL)
-		{
-			printf("%d", *(int *)a_head->content);
-			a_head = a_head->next;
-		}
-		else
-			printf(" ");
-		if (b_head != NULL)
-		{
-			printf(" %d", *(int *)b_head->content);
-			b_head = b_head->next;
-		}
-		else
-			printf(" ");
-		printf("\n");
-	}
+    printf("\n\na b\n___\n");
+    while (wlcount--)
+    {
+        if (a_head != NULL)
+        {
+            printf("%d", *(int *)a_head->content);
+            a_head = a_head->next;
+        }
+        else
+            printf(" ");
+        if (b_head != NULL)
+        {
+            printf(" %d", *(int *)b_head->content);
+            b_head = b_head->next;
+        }
+        else
+            printf(" ");
+        printf("\n");
+    }
 }
+
 int main(int argc, char *argv[])
 {
-	t_list	*a_head;
-	t_list	*b_head;
-	int     i;
-	int		j;
-	int		cmcount = 0;
-	char	*operation;
-	char	**commands = ft_calloc((argc - 1), sizeof(char *));
-	int		pbatoi[argc - 1];
+    t_list  *a_head;
+    t_list  *b_head;
+    int     i;
+    int     j;
+    char    **split_args;
+    int     *val;
 
-
-	operation = "pa pb ra rb rr rra rrb rrr sa sb ss";
-	i = 1;
-	j = 0;
-	a_head = NULL;
-	b_head = NULL;
-	int wlcount;
-
-	wlcount = 20;
-	while (i < argc)
-	{
-		if ((swp_strnstr(operation, argv[i], ft_strlen(operation))) == 1)
-		{
-			commands[cmcount] = argv[i];
-			cmcount++;
-		}
-		else
-		{
-			pbatoi[i] = ft_atoi(argv[i]);
-			ft_lstadd_back(&a_head, ft_lstnew(&pbatoi[i]));
-		}
-		i++;
-	}
-	j = 0;
-
-	while (j < cmcount)
-	{
-		//controls(commands[j], &a_head, &b_head);
-		j++;
-	}
-	//bublesort(&a_head);
-	insertion_sort(&a_head, &b_head);
-	test_print(a_head, b_head);
-	return (0);
+    if (argc < 2)
+        return (0);
+    i = 1;
+    a_head = NULL;
+    b_head = NULL;
+    while (i < argc)
+    {
+        split_args = ft_split(argv[i], ' ');
+        j = 0;
+        while (split_args[j])
+        {
+            val = (int *)malloc(sizeof(int));
+            if (!val)
+                return (1);
+            *val = ft_atoi(split_args[j]);
+            ft_lstadd_back(&a_head, ft_lstnew(val));
+            free(split_args[j]);
+            j++;
+        }
+        free(split_args);
+        i++;
+    }
+    insertion_sort(&a_head, &b_head);
+    test_print(a_head, b_head);
+    return (0);
 }
