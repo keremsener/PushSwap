63e32236805fb9800fc0c97de337b467cb87bce4
ksener <blogkeremsener@gmail.com>
2026-03-05 14:33:51 +0300

    Finished: selectionsort.c

--- src/sorts/simple/selectionsort.c
+++ src/sorts/simple/selectionsort.c
@@ -6,7 +6,7 @@
 /*   By: ksener <ksener@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/05 11:27:06 by ksener            #+#    #+#             */
-/*   Updated: 2026/03/05 13:56:35 by ksener           ###   ########.fr       */
+/*   Updated: 2026/03/05 14:33:02 by ksener           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,11 @@ void	selectionsort(t_list **a_head, t_list **b_head)
 		return ;
 	while ((*a_head) != NULL)
 	{
-		if ((*a_head)->next == NULL)
-		{
-			pb(b_head, a_head);
-			break ;
-		}
 		min_val = get_min_value(*a_head);
-		if (*(int *)(*a_head)->next->content == min_val)
-			pb(b_head, &(*a_head)->next);
+		if (*(int *)(*a_head)->content == min_val)
+			pb(b_head, a_head);
 		else
-		{
-			ra(a_head);
-		}	
+			ra(a_head);	
 	}
 	while (*b_head)
 		pa(a_head, b_head);
