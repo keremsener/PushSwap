d820ddb8aef605fce8a8184b6c2875a4242d4cb3
IIAydedeII <ahmetyasindd@gmail.com>
2026-03-24 15:32:20 +0300

    Reworked insertion_sort

--- src/sorts/simple/instertion_sort.c
+++ src/sorts/simple/instertion_sort.c
@@ -3,39 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   instertion_sort.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ksener <ksener@student.42kocaeli.com.tr    +#+  +:+       +#+        */
+/*   By: adede <adede@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/28 00:53:57 by ksener            #+#    #+#             */
-/*   Updated: 2026/03/17 11:25:53 by ksener           ###   ########.fr       */
+/*   Updated: 2026/03/24 15:31:40 by adede            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	forwhile(t_list **a_head, t_list **b_head, int b_size, int max_val, t_op_count *ops)
+static void	for_while(t_list **a_head, t_list **b_head, t_op_count *ops)
 {
+	int	max;
+	int	size;
+	int	pushed;
+
 	while (*a_head)
 	{
-		max_val = get_int(get_max_node(*b_head));
-		while (max_val != get_int(*b_head))
-		{
+		max = get_int(get_max_node(*b_head));
+		while (get_int(*b_head) != max)
 			rb(b_head, ops);
-		}
-		b_size = ft_lstsize(*b_head);
-		while (*b_head)
+		size = ft_lstsize(*b_head);
+		pushed = 0;
+		while (size--)
 		{
-			if ((get_int(*a_head) > get_int(*b_head))
-				|| b_size <= 0)
+			if (get_int(*a_head) > get_int(*b_head))
 			{
 				pb(b_head, a_head, ops);
+				pushed = 1;
 				break ;
 			}
-			else
-			{
-				rb(b_head, ops);
-				b_size--;
-			}
+			rb(b_head, ops);
 		}
+		if (!pushed)
+			pb(b_head, a_head, ops);
 	}
 }
 
@@ -51,11 +52,12 @@ void	insertion_sort(t_list **a_head, t_list **b_head, t_op_count *ops)
 	if (get_int(*a_head) > get_int((*a_head)->next))
 		sa(a_head, ops);
 	pb(b_head, a_head, ops);
-	forwhile(a_head, b_head, b_size, max_val, ops);
-	max_val = get_int(get_max_node(*b_head));
-	while (max_val != get_int(*b_head))
+	for_while(a_head, b_head, ops);
+	if (*b_head)
 	{
-		rb(b_head, ops);
+		max_val = get_int(get_max_node(*b_head));
+		while (max_val != get_int(*b_head))
+			rb(b_head, ops);
 	}
 	while (*b_head)
 		pa(a_head, b_head, ops);
